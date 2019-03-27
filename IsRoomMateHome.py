508d5406807abf62b2512f17bd22ca9a099514fc
Tek5x <Kyle5@iastate.edu>
2019-03-27 15:53:50 -0500

    Added isroomatehome test code

--- IsRoomMateHome.py
+++ IsRoomMateHome.py
@@ -1,4 +1,3 @@
-import http.client
+import requests
 
-conn = http.client.HTTPSConnection('enwemnmn9zugj.x.pipedream.net')
-conn.request("POST", "/", '{ "name": "Han Solo" }', {'Content-Type': 'application/json'})
+r = requests.post('https://enzf4u4u1gjl7.x.pipedream.net', data = {'key':'kek'})
