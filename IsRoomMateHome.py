65a81303e883b34cf942809b24c721a6f6239a87
Tek5x <Kyle5@iastate.edu>
2019-04-17 15:02:37 -0500

    Added Adafruit

--- IsRoomMateHome.py
+++ IsRoomMateHome.py
@@ -6,12 +6,14 @@ def check_if_home(name):
     group = 'piheads-smart-display'
     seanUrl = 'https://io.adafruit.com/api/v2/{}/groups/{}?X-AIO-Key={}'.format(username, group, SEAN_AIO_KEY)
     samUrl = 'https://io.adafruit.com/api/v2/Zeldatwili/feeds/sam?X-AIO-Key=e546c305d7ef4988867626244f52ce01'
+    kyleUrl = 'https://io.adafruit.com/api/v2/Tek5x/feeds/imnotnamingitwhateveriwant?X-AIO-Key=16270dda24cc4cbc91c72154811a5999'
 
     seanResponse = requests.get(seanUrl)
     seanResponse.raise_for_status()
     samResponse = requests.get(samUrl)
     samResponse.raise_for_status()
-
+    kyleResponse = requests.get(kyleUrl)
+    kyleResponse.raise_for_status()
     # Load JSON data into a Python variable
     seanData = json.loads(seanResponse.text)
     samData = json.loads(seanResponse.text)
